03598e695153e509e0b2a34181f46ba638b08144
mrinmoyborah <mrinmoyborah26100@gmail.com>
2026-03-07 20:26:40 +0530

    POST method

--- src/core/http-client.js
+++ src/core/http-client.js
@@ -39,3 +39,43 @@ export async function sendGET(url, options = {}){
     }
 }
 
+export async function sendPOST(url, options = {}){
+    const config = {
+        headers: {},
+        timeout: 10000
+    }
+
+    if(options.headers){
+        options.headers.forEach(headerString=>{
+            const [key, ...rest] = headerString.split(":");
+            config.headers[key.trim()] = rest.join(":").trim()
+        })
+    }
+
+    if(options.token){
+        config.headers["Authorization"] = `Bearer ${options.token}`
+    }
+
+    let parsedBody = {}
+
+    if(options.body){
+        try{
+            parsedBody = JSON.parse(options.body)
+        }catch(error){
+            throw new Error('Invalid JSON body. Use double quotes: {"example":"octopus"}');
+        }
+    }
+
+    try{
+        const response = await axios.post(url, parsedBody, config);
+        return response;
+    }catch(error){
+        if(error.response){
+            throw error
+        }else if(error.request){
+            throw new Error("No response from server. Check the URL or your internet.");
+        }else{
+            throw new Error(error.message)
+        }
+    }
+}
