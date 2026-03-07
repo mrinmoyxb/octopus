31ed9ddbc78cc09846b6b7376c72aa49e531a17b
mrinmoyborah <mrinmoyborah26100@gmail.com>
2026-03-07 19:51:28 +0530

    GET method using axios

---
+++ src/core/http-client.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+export async function sendGET(url, options = {}){
+    const config = {
+        headers: {},
+        timeout: 10000
+    }
+
+    if(options.headers){
+        options.headers.forEach(headerString => {
+            const [key, ...rest] = headerString.split(":")
+            config.headers[key.trim()] = rest.join(":").trim()
+        })
+    }
+
+    if(options.token){
+        config.headers["Authorization"] = `Bearer ${options.token}`
+    }
+
+    if(options.params){
+        config.params = {}
+        options.params.forEach(paramString => {
+            const [key, value] = paramString.split("=")
+            config.params[key.trim()] = value.trim()
+        })
+    }
+
+    try{
+        const response = await axios.get(url, config);
+        return response;
+    }catch(error){
+        if(error.response){
+            throw error
+        }else if(error.request){
+            throw new Error("No response from server. Check the URL or your internet.");
+        }else{
+            throw new Error(error.message)
+        }
+    }
+}
+
